e2e9112c1bc8b4bfa541203552716b5b996c4e7f
Nicholas Pung <nicholaspung>
2019-05-31 10:33:00 -0700

    added a basic dog app that fetches data from dog api

--- dogs/src/App.js
+++ dogs/src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import DogList from './components/DogList';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      doggos: []
+    }
+  }
+
+  componentDidMount () {
+    this.getDogs('https://dog.ceo/api/breed/hound/images')
+  }
+
+  getDogs = URL => {
+    // feel free to research what this code is doing.
+    // At a high level we are calling an API to fetch some starwars data from the open web.
+    // We then take that data and resolve it our state.
+    fetch(URL)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        this.setState({
+          doggos: data.message.splice(0, 10)
+        });
+      })
+      .catch(err => {
+        throw new Error(err);
+      });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <DogList dogImgURLs={this.state.doggos}/>
+      </div>
+    )
+  }
 }
 
 export default App;
